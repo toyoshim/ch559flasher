390b62c0ac31410905f9e85dc22b40f4ea632bc1
Takashi Toyoshima <toyoshim@gmail.com>
2022-11-03 03:08:30 +0900

    implement detection

--- src/ch559.rs
+++ src/ch559.rs
@@ -7,6 +7,7 @@ pub struct Ch559 {
     handle: Option<rusb::DeviceHandle<rusb::GlobalContext>>,
     ep_in: u8,
     ep_out: u8,
+    chip_id: u8,
 }
 
 impl Ch559 {
@@ -17,6 +18,7 @@ impl Ch559 {
             handle: rusb::open_device_with_vid_pid(VID, PID),
             ep_in: 0,
             ep_out: 0,
+            chip_id: 0,
         };
         if ch559.is_connected() {
             if let Err(error) = ch559.initialize() {
@@ -46,20 +48,28 @@ impl Ch559 {
                     interface_number = interface.number();
                     if let Some(desc) = interface.descriptors().next() {
                         let mut ep_in_found = false;
+                        let mut ep_in_type = rusb::TransferType::Bulk;
                         let mut ep_out_found = false;
+                        let mut ep_out_type = rusb::TransferType::Bulk;
                         for ep in desc.endpoint_descriptors() {
                             match ep.direction() {
                                 rusb::Direction::In => {
-                                    self.ep_in = ep.number();
+                                    self.ep_in = ep.address();
+                                    ep_in_type = ep.transfer_type();
                                     ep_in_found = true;
                                 }
                                 rusb::Direction::Out => {
-                                    self.ep_out = ep.number();
+                                    self.ep_out = ep.address();
+                                    ep_out_type = ep.transfer_type();
                                     ep_out_found = true;
                                 }
                             }
                         }
-                        if !ep_in_found || !ep_out_found {
+                        if !ep_in_found
+                            || !ep_out_found
+                            || ep_in_type != rusb::TransferType::Bulk
+                            || ep_out_type != rusb::TransferType::Bulk
+                        {
                             return Err(String::from("failed to detect EPs"));
                         }
                     }
@@ -75,9 +85,46 @@ impl Ch559 {
             if let Err(_) = handle.claim_interface(interface_number) {
                 return Err(String::from("failed to claim the target interface"));
             }
-            // TODO: detect
+            let detect_request = [
+                0xa1, 0x12, 0x00, 0x59, 0x11, 0x4d, 0x43, 0x55, 0x20, 0x49, 0x53, 0x50, 0x20, 0x26,
+                0x20, 0x57, 0x43, 0x48, 0x2e, 0x43, 0x4e,
+            ];
+            let mut detect_response: [u8; 6] = [0; 6];
+            match self.send_receive(&detect_request, &mut detect_response) {
+                Ok(_) => {
+                    if detect_response[4] != 0x59 {
+                        return Err(String::from("failed to receive a valid response"));
+                    }
+                    self.chip_id = detect_response[4];
+                }
+                Err(string) => return Err(string),
+            };
+
             // TODO: identify
+            return Ok(());
+        } else {
+            return Err(String::from("invalid handle"));
         }
-        return Ok(());
+    }
+
+    fn send_receive(&mut self, request: &[u8], response: &mut [u8]) -> Result<(), String> {
+        if let Some(handle) = &mut self.handle {
+            if let Ok(size) =
+                handle.write_bulk(self.ep_out, request, core::time::Duration::new(1, 0))
+            {
+                if size != request.len() {
+                    return Err(String::from("failed to do a bulk write all data"));
+                }
+            } else {
+                return Err(String::from("failed to do a bulk write"));
+            }
+            if let Ok(_) = handle.read_bulk(self.ep_in, response, core::time::Duration::new(1, 0)) {
+                return Ok(());
+            } else {
+                return Err(String::from("failed to do a bulk read response"));
+            }
+        } else {
+            return Err(String::from("invalid handle"));
+        }
     }
 }
