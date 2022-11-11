397b9587d8b18edb838b866e29a2b572307c0683
Takashi Toyoshima <toyoshim@gmail.com>
2022-11-11 22:26:17 +0900

    update copyright header format

--- src/ch559/progress_bar.rs
+++ src/ch559/progress_bar.rs
@@ -1,4 +1,4 @@
-// Copyright 2022 Takashi Toyoshima <toyoshim@gmail.com>. All rights reserved.
+// Copyright 2022 Takashi Toyoshima <toyoshim@gmail.com>.
 // Use of this source code is governed by a BSD-style license that can be found
 // in the LICENSE file.
 use std::io::{stdout, Write};
